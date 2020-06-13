2e2a2dbf3f222791b9c9857dd3e7c5a56b060a4f
Adenikym <Adenikym@users.noreply.github.com>
2020-06-13 17:14:28 +0100

    MY FOLDER

---
+++ adenikyRepo-master/script.js
@@ -0,0 +1,8 @@
+// Navabr script
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+navToggle.addEventListener("click", () => links.classList.toggle("show-links"));
+
+
+
+// Facilities css
